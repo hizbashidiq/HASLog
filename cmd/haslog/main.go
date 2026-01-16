0372927ae191453a8af0ecddd3e1e4938be37295
hizbashidiq <hizbulloh.ash@gmail.com>
2026-01-16 13:05:25 +0700

    feat: add environment variables

--- cmd/haslog/main.go
+++ cmd/haslog/main.go
@@ -5,12 +5,24 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"fmt"
+	"os"
 
 	"github.com/hizbashidiq/HASLog/internal/api"
 	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	_ = godotenv.Load()
+
+	DB_URL := fmt.Sprintf("postgres://%s:%s@localhost:%s/%s",
+				os.Getenv("DB_USER"),
+				os.Getenv("DB_PASSWORD"),
+				os.Getenv("DB_PORT"),
+				os.Getenv("DB_NAME"))	
+
 	db, err := sql.Open("pgx", DB_URL)
 	if err!=nil{
 		log.Fatal(err)
@@ -22,5 +34,5 @@ func main() {
 	api.Setup(db, timeout)
 
 	log.Println("Starting web server...")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(os.Getenv("SERVER_ADDRESS"), nil)
 }
